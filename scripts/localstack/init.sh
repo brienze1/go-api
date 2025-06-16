9a4021be8f8bcf3abbe98d37abdb65abe8eb1cb0
Luis Brienze <lfbrienze@gmail.com>
2025-06-16 18:20:35 +0100

    add AWS configuration and resource creation to init script

--- scripts/localstack/init.sh
+++ scripts/localstack/init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "########### Creating profile ###########"
+
+aws configure set aws_access_key_id default_access_key
+aws configure set aws_secret_access_key default_secret_key
+aws configure set region us-east-2
+
+echo "########### Creating resources ###########"
+
 aws --endpoint-url=http://localhost:4566 secretsmanager create-secret \
   --name notes-database-secret \
   --secret-string '{"username":"postgres","password":"postgres","engine":"postgres","host":"localhost","port":5432,"dbname":"postgres","dbInstanceIdentifier":"notes"}' \
